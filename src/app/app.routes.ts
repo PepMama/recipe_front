d09c2003224babc69e36bc5d7391edd1cd3dc247
Maelic Pepin <pmaelic@outlook.fr>
2025-07-27 23:27:54 +0200

    PM - :sparkles: design form register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CreateUser } from './pages/create-user/create-user';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: CreateUser }
+];
--- src/app/pages/create-user/create-user.ts
+++ src/app/pages/create-user/create-user.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './create-user.html',
-  styleUrl: './create-user.css'
+  styleUrls: ['./create-user.css']
 })
 export class CreateUser {
+  // Créer le formulaire angular
+  createUserForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
 
+  // Méthode pour soumettre le formulaire
+  onSubmit() {
+    const formData = this.createUserForm.value;
+    console.log('Form Data:', formData);
+  }
 }
