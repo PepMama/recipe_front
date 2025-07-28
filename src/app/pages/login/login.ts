e639abc9ae1d0b103a131e1cb8e8820d052b2019
Maelic Pepin <pmaelic@outlook.fr>
2025-07-28 10:52:46 +0200

    PM - :sparkles: Implement user authentication with login and registration forms

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth';
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule, 
+    CommonModule,
+    RouterModule,
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
 export class Login {
+  loginUserForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private auth: AuthService){
+    this.loginUserForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if(this.loginUserForm.invalid) {
+      this.loginUserForm.markAllAsDirty();
+      return
+    }
+
+    const { ...userData } = this.loginUserForm.value;
+
+    this.auth.login(userData).subscribe({
+      next:(response) =>{
+        console.log('Connexion réussie', response);
+      },
+      error: (err) => {
+        console.error('Erreur lors de la connexion', err);
+      }
+    })
+  }
 }
