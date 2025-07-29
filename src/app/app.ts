b7747039b19f1c49fa831c49a0688d09dc7fc7b0
Maelic Pepin <pmaelic@outlook.fr>
2025-07-29 17:12:50 +0200

    PM - :sparkles: design navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,27 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet }       from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector:  'app-root',
   standalone: true,
-  imports:   [ RouterOutlet ],
+  imports: [
+    RouterOutlet, 
+    Navbar, 
+    CommonModule
+  ],
   templateUrl: './app.html',
   styleUrls:   ['./app.css']
 })
 export class App {
-  protected readonly title = signal('recipe_front');
+  isAuthPage = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      const url = this.router.url;
+      this.isAuthPage = url === '/login' || url === '/create-user';
+    });
+  }
 }
